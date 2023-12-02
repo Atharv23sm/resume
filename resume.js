94a610aa93218cf927527542ea05bdc4226a0495
Atharv Mahabal <122306441+Atharv23sm@users.noreply.github.com>
2023-12-03 00:14:05 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,20 @@
+
+var prev_scroll = window.pageYOffset;
+window.onscroll = function () {
+    var cur_scroll = window.pageYOffset;
+    if (prev_scroll > cur_scroll) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-5vw";
+    }
+    prev_scroll = cur_scroll;
+}
+
+function onClickTransition(element) {
+    element.classList.toggle('afterclick');
+}
+
+function onClickSect(sectid) {
+    var div = document.getElementById(sectid);
+    div.classList.toggle('afterclick');
+}
